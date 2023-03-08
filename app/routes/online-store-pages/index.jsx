732c178314cf7e6406d353652027360928047239
davecyen@gmail.com <davecyen@gmail.com>
2023-03-08 10:47:54 -0800

    Initial commit

---
+++ app/routes/online-store-pages/index.jsx
@@ -0,0 +1,17 @@
+export async function loader({request}) {
+  const response = await fetch(
+    'https://{shop}.myshopify.com/pages/' + 'about',
+    {
+      headers: {
+        // It's important to forward these headers. Otherwise, the online store might block the request.
+        'X-Shopify-Client-IP': request.headers.get('X-Shopify-Client-IP'),
+        'X-Shopify-Client-IP-Sig': request.headers.get(
+          'X-Shopify-Client-IP-Sig',
+        ),
+        'User-Agent': 'Hydrogen',
+      },
+    },
+  );
+  const data = await response.text();
+  return new Response(data, {headers: {'content-type': 'text/html'}});
+}
